fcdd24694ebdb768e3b8dc9337876a3d045f977a
Hui Qiao <huiqiao@bitorobotics.ltd>
2022-01-23 13:02:29 +0800

    users

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2022/1/23 11:59 上午
+@Author  : QiAoHui
+@Email   : qiaohui@bitorobotics.ltd
+@File    : urls
+@Version : V0.0.1
+@license : (C) Copyright 2017-2030, Bito Robotics Co.Ltd.
+"""
+from .models import *
+from utils.json_decorator import render_to_json
 
-# Create your views here.
+
+@render_to_json
+def user_set(request):
+    data = User.objects.all()
+    data = [{"id": elem.id, "name": elem.name} for elem in data]
+    return data
